dd9a273cc1ea93c52d36cf2f86fcb94637f05f36
Owen Ceresa <co84635@mail.harpercollege.edu>
2024-10-05 13:46:14 -0500

    Cheese

--- src/main/java/com/csc/CheeseAnalyzer.java
+++ src/main/java/com/csc/CheeseAnalyzer.java
@@ -2,24 +2,26 @@ package com.csc;
 import java.io.*;
 
 public class CheeseAnalyzer {
+  @SuppressWarnings("resource")
   public static void main(String[] args) {
     int amountOfPasteurizedMilk = 0;
     int amountOfRawMilk = 0;
-    int amountOfOrganicGreaterThanFourtyOnePercent = 0;
+    int amountOfOrganicGreaterThanFortyOnePercent = 0;
+
     String mostCommonTypeOfAnimal = "";
     int amountOfCows = 0;
     int amountOfGoats = 0;
     int amountOfEwe = 0;
     int amountOfBuffalo = 0;
-    
 
     String csvFile = "src\\cheese_data.csv";
-    BufferedReader reader = null;
-    String line = "";
-    String [] row;
 
+    BufferedReader reader = null;
+       
     try {
       reader = new BufferedReader(new FileReader(csvFile));
+      String line = "";
+      int lineNumber = 0;
 
       while((line = reader.readLine()) != null) {
         line = line.trim();
@@ -28,24 +30,32 @@ public class CheeseAnalyzer {
           continue;
         }
 
-        row = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
+        lineNumber++;
+
+        if(lineNumber == 1) {
+          continue;
+        }
+
+        String[] column = line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)");
         
-        if ("Pasteurized".equals(row[9])) {
+        if ("Pasteurized".equals(column[9])) {
           amountOfPasteurizedMilk++;
-        } else if ("Raw Milk".equals(row[9])) {
+        } else if ("Raw Milk".equals(column[9])) {
           amountOfRawMilk++;
         }
 
-        // String moisturePercentage = row[3].trim();
-        
-        // if(!moisturePercentage.isEmpty()) {
-        //   int moisturePercent = Integer.parseInt(moisturePercentage);
-        //   if ("1".equals(row[6].trim()) && moisturePercent > 41.0) {
-        //     amountOfOrganicGreaterThanFourtyOnePercent++;
-        //   }
-        // }
-        
-        switch (row[8]) {
+        boolean isOrganic = column[6].equals("1");
+        if(isOrganic) {
+          try{
+            Double moisturePercentage = Double.parseDouble(column[3].trim());
+            if(moisturePercentage >= 41){
+              amountOfOrganicGreaterThanFortyOnePercent++;
+            }
+        } catch (NumberFormatException e) {
+        }
+      }
+      
+        switch (column[8]) {
           case "Cow":
             amountOfCows++;
             break;
@@ -61,24 +71,13 @@ public class CheeseAnalyzer {
           default:
             break;      
         }
-
       }
 
     } catch (Exception e) {
       e.printStackTrace();
-    } finally {
-        try{
-          reader.close();
-        } catch (IOException e) {
-          e.printStackTrace();
-        }
-      } 
-
-    System.out.println(amountOfPasteurizedMilk);
-    System.out.println(amountOfRawMilk);
-    System.out.println(amountOfOrganicGreaterThanFourtyOnePercent);
-    
-
+      return;
+    } 
+       
     int[] amountOfAnimals = {amountOfCows, amountOfGoats,  amountOfEwe, amountOfBuffalo,};
     String[] animalTypes = {"Cow", "Goat", "Ewe", "Buffalo Cows"};
     int largestAnimalCount = amountOfAnimals[0];
@@ -86,10 +85,19 @@ public class CheeseAnalyzer {
 
     for (int i = 1; i < amountOfAnimals.length; i++) {
       if (amountOfAnimals[i] > largestAnimalCount) {
+        largestAnimalCount = amountOfAnimals[i];
         mostCommonTypeOfAnimal = animalTypes[i];
       }
     }
 
-    System.out.println("Most Common Animal: " + mostCommonTypeOfAnimal);
+    try (FileWriter writer = new FileWriter("output.txt")) {
+      writer.write("Amount of Pasteurized Milk: " + amountOfPasteurizedMilk + '\n');
+      writer.write("Amount of Raw Milk: " + amountOfRawMilk + '\n');
+      writer.write("Amount of Organic Cheese greater than 41%: " + amountOfOrganicGreaterThanFortyOnePercent + '\n');
+      writer.write("Most Common Animal: " + mostCommonTypeOfAnimal);
+    } catch (IOException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
+    }
   }
 }
